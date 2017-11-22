e29e216ef9889a6e32fcf6d000431dff7cb6193b
Alessandro <aleferrara1998@gmail.com>
2017-11-22 19:44:59 +0100

    added project logo and improved output messages

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 //Watering time max/min constant (minutes)
-#define WATERING_TIME_AT_0_DEGREES 0
-#define WATERING_TIME_AT_30_DEGREES 20
+#define WATERING_TIME_AT_0_DEGREES 0.0
+#define WATERING_TIME_AT_30_DEGREES 20.0
 
 
 //Plants angle (E=0, N=90, W=180, S=270,)
@@ -26,7 +25,7 @@ int wind_incidence_function(float wind_direction, int time);
 
 
 //Declaring global typedef array of structs
-typedef struct weather_struct
+typedef struct
 	{
 		int temperature;
 		float wind_speed;
@@ -34,7 +33,7 @@ typedef struct weather_struct
 		int weather_id;
 		int wind_incidence;
 		float watering_multiplier;
-		int watering_time
+		int watering_time;
 	} weather_data_struct;	
 
 weather_data_struct weather[HOURS]; //given for 9:00, 12:00, 15:00, 18:00, 21:00, 24:00, 3:00, 6:00 if fetched at 8:00 in the morning.
@@ -45,7 +44,17 @@ int main(void) {
 	//Declaring variables
 	float watering_time_per_degree;
 
-	printf("____START CALCULATING IRRIGATION TIME____\n\n");
+	printf("______ _      ______          _           _   \n");
+	printf("| ___ (_)     | ___ \\        (_)         | |  \n");
+	printf("| |_/ /_  __ _| |_/ / __ ___  _  ___  ___| |_ \n");
+	printf("|  __/| |/ _` |  __/ '__/ _ \\| |/ _ \\/ __| __|\n");
+	printf("| |   | | (_| | |  | | | (_) | |  __/ (__| |_ \n");
+	printf("\\_|   |_|\\__, \\_|  |_|  \\___/| |\\___|\\___|\\__|\n");
+	printf("          __/ |             _/ |              \n");
+	printf("         |___/             |__/               \n\n");
+
+
+	printf("----START CALCULATING IRRIGATION TIME----\n\n");
 
 	//Calculate watering_time_per_degree based on constants (#define)
 	watering_time_per_degree = (WATERING_TIME_AT_30_DEGREES - WATERING_TIME_AT_0_DEGREES)/30;
@@ -108,7 +117,7 @@ float watering_multiplier(int weather_id, int time, int wind_incidence, float wi
 		return 0;
 	}
 	if (weather_id == 904) { //HOT
-		return 1,25;
+		return 1.25;
 	}
 	if ((weather_id >= 900) && (weather_id <= 902)) { //TORNADO/HURRYCANE
 		return 0;
@@ -119,34 +128,44 @@ float watering_multiplier(int weather_id, int time, int wind_incidence, float wi
 
 	if ((weather_id >= 200) && (weather_id <= 232) && (wind_incidence_function(wind_direction, time))) {
 		if (weather_id == 200) { //THUNDERSTORM WITH LIGHT RAIN
-			return 0,70;
+			printf("Weather is: THUNDERSTORM WITH LIGHT RAIN\n");
+			return 0.70;
 		}
-		if (weather_id == 201) { //THUNDERSTORM WITH RAIN
-			return 0,50;
+		if (weather_id == 201) {
+			printf("Weather is: THUNDERSTORM WITH RAIN\n");//THUNDERSTORM WITH RAIN
+			return 0.50;
 		}
 		if (weather_id == 202) { //THUNDERSTORM WITH HEAVY RAIN
-			return 0,25;
+			printf("Weather is: THUNDERSTORM WITH HEAVY RAIN\n");
+			return 0.25;
 		}
 		if (weather_id == 210) { //LIGHT THUNDERSTORM
-			return 0,90;
+			printf("Weather is: LIGHT THUNDERSTORM\n");
+			return 0.90;
 		}
 		if (weather_id == 211) { //THUNDERSTORM
-			return 0,80;
+			printf("Weather is: THUNDERSTORM\n");
+			return 0.80;
 		}
 		if (weather_id == 212) { //HEAVY THUNDERSTORM
-			return 0,60;
+			printf("Weather is: HEAVY THUNDERSTORM\n");
+			return 0.60;
 		}
 		if (weather_id == 221) { //RUGGED THUNDERSTORM (Temporale irregolare)
-			return 0,70;
+			printf("Weather is: RUGGED THUNDERSTORM\n");
+			return 0.70;
 		}
 		if (weather_id == 230) { //THUNDERSTORM WITH LIGHT DRIZZLE
-			return 0,80;
+			printf("Weather is: THUNDERSTORM WITH LIGHT DRIZZLE\n");
+			return 0.80;
 		}
 		if (weather_id == 231) { //THUNDERSTORM WITH DRIZZLE
-			return 0,70;
+			printf("Weather is: THUNDERSTORM WITH DRIZZLE\n");
+			return 0.70;
 		}
 		if (weather_id == 232) { //THUNDERSTORM WITH HEAVY DRIZZLE
-			return 0,55;
+			printf("Weather is: THUNDERSTORM WITH HEAVY DRIZZLE\n");
+			return 0.55;
 		}
 	}
 
@@ -155,31 +174,40 @@ float watering_multiplier(int weather_id, int time, int wind_incidence, float wi
 
 	if ((weather_id >= 300) && (weather_id <= 321) && (wind_incidence_function(wind_direction, time))) {
 		if (weather_id == 300) { //LIGHT INTENSITY DRIZZLE
-			return 0,90;
+			printf("Weather is: LIGHT INTENSITY DRIZZLE\n");
+			return 0.90;
 		}
 		if (weather_id == 301) { //DRIZZLE
-			return 0,80;
+			printf("Weather is: DRIZZLE\n");
+			return 0.80;
 		}
 		if (weather_id == 302) { //HEAVY INTENSITY DRIZZLE
-			return 0,50;
+			printf("Weather is: HEAVY INTENSITY DRIZZLE\n");
+			return 0.50;
 		}
 		if (weather_id == 310) { //LIGHT INTENSITY DRIZZLE RAIN
-			return 0,65;
+			printf("Weather is: LIGHT INTENSITY DRIZZLE RAIN\n");
+			return 0.65;
 		}
 		if (weather_id == 311) { //DRIZZLE RAIN
-			return 0,50;
+			printf("Weather is: DRIZZLE RAIN\n");
+			return 0.50;
 		}
 		if (weather_id == 312) { //HEAVY INTENSITY DRIZZLE RAIN
-			return 0,40;
+			printf("Weather is: HEAVY INTENSITY DRIZZLE RAIN\n");
+			return 0.40;
 		}
 		if (weather_id == 313) { //SHOWER RAIN AND DRIZZLE
-			return 0,30;
+			printf("Weather is: SHOWER RAIN AND DRIZZLE\n");
+			return 0.30;
 		}
 		if (weather_id == 314) { //HEAVY SHOWER RAIN AND DRIZZLE
-			return 0,20;
+			printf("Weather is: HEAVY SHOWER RAIN AND DRIZZLE\n");
+			return 0.20;
 		}
 		if (weather_id == 321) { //SHOWER DRIZZLE
-			return 0,35;
+			printf("Weather is: SHOWER DRIZZLE\n");
+			return 0.35;
 		}
 	}
 
@@ -188,26 +216,34 @@ float watering_multiplier(int weather_id, int time, int wind_incidence, float wi
 
 	if ((weather_id >= 500) && (weather_id <= 520) && (wind_incidence_function(wind_direction, time))) {
 		if (weather_id == 500) { //LIGHT RAIN
-			return 0,80;
+			printf("Weather is: LIGHT RAIN\n");
+			return 0.80;
 		}
 		if (weather_id == 501) { //MODERATE RAIN
-			return 0,70;
+			printf("Weather is: MODERATE RAIN\n");
+			return 0.70;
 		}
 		if (weather_id == 502) { //HEAVY INTENSITY RAIN
-			return 0,40;
+			printf("Weather is: HEAVY INTENSITY RAIN\n");
+			return 0.40;
 		}
 		if (weather_id == 503) { //VERY HEAVY RAIN
-			return 0,30;
+			printf("Weather is: VERY HEAVY RAIN\n");
+			return 0.30;
 		}
 		if (weather_id == 504) { //EXTREME RAIN
-			return 0,10;
+			printf("Weather is: EXTREME RAIN\n");
+			return 0.10;
 		}
 		if (weather_id == 511) { //FREEZING RAIN
+			printf("Weather is: FREEZING RAIN\n");
 			return 0;
 		}
 		if ((weather_id >= 520) && (weather_id <= 531)) { //LIGHT INTNENSITY SHOWER RAIN/SHOWER RAIN/RAGGED SHOWER RAIN
+			printf("Weather is: LIGHT INTENSITY SHOWER RAIN/SHOWER RAIN/RAGGED SHOWER RAIN\n");
 			return 0;
 		}
 	}
+	printf("Weather is: NOT DEFINED\n");
 	return 1;
 }
