9a68f0289175630ffac3a5d743ce0e4c1e77445d
Alessandro <aleferrara1998@gmail.com>
2017-12-24 21:42:34 +0100

    license add

--- src/weather_fetcher.cpp
+++ src/weather_fetcher.cpp
@@ -1,3 +1,25 @@
+/*
+  Copyright (c) 2017-2018 Alessandro Ferrara and Andrea Taglia
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
 #include <atomic>
 #include <stdio.h>
 #include <iostream>
@@ -11,7 +33,7 @@ using namespace Pistache::Http;
 
 
 WeatherFetcher::WeatherFetcher(){
-    std::cout << "weather_fetcher ready" << std::endl;
+    std::cout << "Weather fetcher ready" << std::endl;
     auto opts = Http::Client::options()
         .threads(1)
         .maxConnectionsPerHost(8);
@@ -25,7 +47,7 @@ WeatherFetcher::~WeatherFetcher(){
 }
 
 void WeatherFetcher::fetchCurrentWeather(std::string &s){
-    std::cout << "making request..." << std::endl;
+    std::cout << "Making request..." << std::endl;
     std::string page = "http://api.openweathermap.org/data/2.5/weather?q=MIlan,it&mode=json&appid=013b5d74095d80de286d5f0d93a85fe5";
     int count = 1;
 
